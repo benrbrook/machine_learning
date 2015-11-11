8df3af5c50fdbe6b4774ff7feccfb3234a60d22e
Ben Brook <bbrook154@gmail.com>
2015-11-10 22:21:01 -0800

    Add train validate test data processing

--- knn/knn.py
+++ knn/knn.py
@@ -1,5 +1,5 @@
 import csv
-import random
+from random import randint
 import math
 import operator
 import sys
@@ -9,79 +9,100 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 # Load in houses and split into training and test sets
-def loadDataset(filename, split, trainingSet=[], test_set=[]):
+def load_dataset(filename, training_set=[], validation_set = [], test_set=[]):
 	with open(filename, 'rt') as csvfile:
 		lines = csv.reader(csvfile)
 		dataset = list(lines)
 		for x in range(1, len(dataset) - 1):
 			# Grab address and price
-			house = dataset[x]
-			if random.random() < split:
-				trainingSet.append(house)
+			data_point = dataset[x]
+			r = randint(1, 3)
+			if r == 1:
+				training_set.append(data_point)
+			elif r == 2:
+				validation_set.append(data_point)
 			else:
-				test_set.append(house)
+				test_set.append(data_point)
 
 # Calculate euclidean between two points
-def euclideanDistance(instance1, instance2, length):
+def euclidean_distance(instance1, instance2, length):
 	distance = 0
 	for x in range(length):
 		distance += pow((float(instance1[x]) - float(instance2[x])), 2)
 	return math.sqrt(distance)
 
 
-# Get euclidean distance to every other house
-def getNeighbors(trainingSet, testInstance, k):
+# Get euclidean distance to k nearest houses
+def get_neighbors(training_set, test_instance, k):
 	distances = []
-	length = len(testInstance) - 1
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(testInstance, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
+	length = len(test_instance) - 1
+	for x in range(len(training_set)):
+		dist = euclidean_distance(test_instance, training_set[x], length)
+		distances.append((training_set[x], dist))
 	distances.sort(key=operator.itemgetter(1))
 	neighbors = []
 	for x in range(k):
 		neighbors.append(distances[x][0])
 	return neighbors
 
-# Poll neighbors and arrange from lowet value to highest value
-def getResponse(neighbors):
-	classVotes = {}
-	for x in range(len(neighbors)):
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-	sortedVotes = sorted(list(classVotes.items()), key=operator.itemgetter(1), reverse=True)
-	return sortedVotes[0][0]
+# Average k nearest neighbors
+def average_neighbors(neighbors):
+	sum = 0
+	for data_point in neighbors:
+		# Add in value
+		sum += int(data_point[-1])
+	return sum/len(neighbors)
 
 # Compare test set to the prediction within margin of error
-def getAccuracy(test_set, predictions):
-	if len(test_set) != len(predictions):
+def get_rmse(validation_set, predictions):
+	if len(validation_set) != len(predictions):
 		print("Sets of different lengths")
 		exit(1)
-
-	squared_errors = 0
-	for i in range(len(test_set)):
-		squared_errors += ((int(predictions[i]) - int(test_set[i])) ** 2)
-	mse = (squared_errors / len(test_set))
-	rmse = sqrt(mse)
+	rmse = sqrt(mean_squared_error(validation_set, predictions))
 	return rmse
 
-trainingSet = []
+def get_accuracy(test_set, predictions, rmse):
+	print(test_set)
+	print(predictions)
+	correct = 0
+	for i in range(len(test_set)):
+		if (test_set[i] - rmse) >= predictions[i] or (test_set[i] + rmse) <= predictions[i]:
+			correct += 1
+	return correct / len(test_set)
+	
+
+training_set = []
+validation_set = []
 test_set = []
-loadDataset(sys.argv[1], 0.67, trainingSet, test_set)
-predictions = []
-rmse = 0
-k = int(sys.argv[2])
+load_dataset(sys.argv[1], training_set, validation_set, test_set)
+
+# Main
+rmse = []
+for k in range(1, 2):
+	print("K: " + str(k))
+	nearest_k_neighbors = []
+	neighbor_average = 0
+	predictions = []
+	for x in range(len(validation_set)):
+		nearest_k_neighbors = get_neighbors(training_set, validation_set[x], k)
+		neighbor_average = average_neighbors(nearest_k_neighbors)
+		predictions.append(neighbor_average)
+	rmse.append((k, get_rmse([int(x[-1]) for x in validation_set], predictions)))
+optimal_model = sorted(rmse, key=operator.itemgetter(1))[0]
+optimal_k = optimal_model[0]
+optimal_rmse = optimal_model[1]
+print("Optimal k: %d, rmse: %.3f" % (optimal_k, optimal_rmse))
+
+final_rmse = 0
+final_predictions = []
 for x in range(len(test_set)):
-	neighbors = getNeighbors(trainingSet, test_set[x], k)
-	result = getResponse(neighbors)
-	predictions.append(result)
-test_set = [x[-1] for x in test_set]
-
-rmse = getAccuracy(test_set, predictions)
-print("k: " + str(k) + " rmse: " + str(rmse))
-
+	final_nearest_k_neighbors = get_neighbors(validation_set, test_set[x], optimal_k)
+	final_neighbor_average = average_neighbors(final_nearest_k_neighbors)
+	final_predictions.append(final_neighbor_average)
+final_rmse = get_rmse([int(x[-1]) for x in test_set], final_predictions)
+acccuracy = get_accuracy([int(x[-1]) for x in test_set], final_predictions, final_rmse)
+print("Final rmse: %.3f" % final_rmse)
+print("Accuracy: %.1f" % acccuracy)
 
 
 
